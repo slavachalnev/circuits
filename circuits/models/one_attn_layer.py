d3e572cc0b161d75960d599496b78fe47165e1f8
Slava Chalnev <slava.chalnev@gmail.com>
2023-01-30 16:38:50 +1100

    Move pos encoding to attn

--- circuits/models/one_attn_layer.py
+++ circuits/models/one_attn_layer.py
@@ -8,16 +8,6 @@ from yacs.config import CfgNode as CN
 from circuits.models.model import Model
 
 
-class AttentionOnlyBlock(nn.Module):
-    def __init__(self, n_embed, n_head, attn_pdrop=0.0):
-        super().__init__()
-        self.attn = nn.MultiheadAttention(n_embed, num_heads=n_head, batch_first=True)
-
-    def forward(self, x):
-        h, _ = self.attn(x, x, x)
-        return x + h
-
-
 class SinusoidalEncoding(nn.Module):
 
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
@@ -41,6 +31,17 @@ class SinusoidalEncoding(nn.Module):
         return self.dropout(x)
 
 
+class AttentionOnlyBlock(nn.Module):
+    def __init__(self, n_embed, n_head, block_size, pos_pdrop=0.0):
+        super().__init__()
+        self.attn = nn.MultiheadAttention(n_embed, num_heads=n_head, batch_first=True)
+        self.pos = SinusoidalEncoding(d_model=n_embed, dropout=pos_pdrop, max_len=block_size)
+
+    def forward(self, x):
+        h, _ = self.attn(x, x, x)
+        return x + h
+
+
 class OneLayerAttnTransformer(Model):
 
     @staticmethod
@@ -55,7 +56,7 @@ class OneLayerAttnTransformer(Model):
         C.n_head = 8
 
         # dropout hyperparameters
-        C.pos_embd_pdrop = 0.1
+        C.pos_embd_pdrop = 0.0
 
         return C
     
@@ -65,13 +66,18 @@ class OneLayerAttnTransformer(Model):
         # embedding
         self.embedding = nn.Embedding(config.vocab_size, config.n_embd)
         # self.pos_embedding = nn.Embedding(config.block_size, config.n_embd)
-        self.pos_embedding = SinusoidalEncoding(
-            d_model=config.n_embd,
-            dropout=config.pos_embd_pdrop,
-            max_len=config.block_size,
-        )
+        # self.pos_embedding = SinusoidalEncoding(
+        #     d_model=config.n_embd,
+        #     dropout=config.pos_embd_pdrop,
+        #     max_len=config.block_size,
+        # )
 
-        self.attn = AttentionOnlyBlock(n_embed=config.n_embd, n_head=config.n_head)
+        self.attn = AttentionOnlyBlock(
+            n_embed=config.n_embd,
+            n_head=config.n_head,
+            block_size=config.block_size,
+            pos_pdrop=config.pos_embd_pdrop,
+            )
         self.unembedding = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
         # initialize weights
@@ -84,10 +90,12 @@ class OneLayerAttnTransformer(Model):
         x = self.embedding(x)
 
         # position embedding
+
         # pos = torch.arange(0, x.size(1), device=x.device).unsqueeze(0) # shape (1, t)
         # pos_emb = self.pos_embedding(pos)
         # x = x + pos_emb
-        x = self.pos_embedding(x)
+
+        # x = self.pos_embedding(x)
 
         # attention layer
         x = self.attn(x)

7305a719d38b17c2f000b2094d267ca42ba303af
Slava Chalnev <slava.chalnev@gmail.com>
2023-02-02 19:21:03 +1100

    Plot eigenvalues

--- experiments/one_layer_attn.py
+++ experiments/one_layer_attn.py
@@ -8,6 +8,8 @@ import torch
 
 import tiktoken
 
+from matplotlib import pyplot as plt
+
 from torch.utils.tensorboard import SummaryWriter
 
 from circuits.models.one_attn_layer import OneLayerAttnTransformer
@@ -28,12 +30,12 @@ def get_config():
     # model
     C.model = OneLayerAttnTransformer.get_default_config()
     C.model.vocab_size = 50257
-    C.model.n_embd =512 #768
-    C.model.n_head =8 #12
+    C.model.n_embd = 768
+    C.model.n_head = 12
 
     # trainer
     C.trainer = Trainer.get_default_config()
-    C.trainer.block_size = 256#2048
+    C.trainer.block_size = 2048
     C.trainer.batch_size = 32
     C.trainer.micro_batch_size = 4
 
@@ -92,51 +94,76 @@ def train():
     trainer.run()
 
 
-def source_to_out_token(source, tokenizer, weights, d_model, n_heads):
-    """ OV circuit. Computes all heads simultaneously. """
+def get_weights_for_head(weights, head, n_heads, d_model, norm_emb=True):
+    """ Get the weights for a single head. """
+    d_head = d_model // n_heads
+
+    w_v = weights['attn.attn.in_proj_weight'][2*d_model:]
+    w_o = weights['attn.attn.out_proj.weight'] 
+
+    w_v_h = w_v[head*d_head: (head+1)*d_head, :]
+    w_o_h = w_o[:, head*d_head: (head+1)*d_head]
+
+
+    ln1w = weights['attn.ln.weight'].unsqueeze(1).numpy()
+    ln1b = weights['attn.ln.bias'].unsqueeze(1).numpy()
+
+    w_e = weights['embedding.weight'].numpy().T
+    if norm_emb:
+        w_e = (w_e - np.average(w_e)) / np.std(w_e)
+        w_e = w_e * ln1w + ln1b
+
+    return {
+        'w_e': w_e,
+        'w_v': w_v_h.numpy(),
+        'w_o': w_o_h.numpy(),
+        'w_u': weights['unembedding.weight'].numpy(),
+        'ln1w': ln1w,
+        'ln1b': ln1b,
+        'lnfw': weights['ln_f.weight'].unsqueeze(1).numpy(),
+        'lnfb': weights['ln_f.bias'].unsqueeze(1).numpy(),
+    }
+
+
+def get_eigenvalues(weights, head, n_heads, d_model):
+    """ Get the eigenvalues for the w_v @ w_e @ w_u @ w_o matrix. """
+    w = get_weights_for_head(weights, head, n_heads, d_model)
+    m = w['w_v'] @ w['w_e'] @ w['w_u'] @ w['w_o']
+    return np.linalg.eigvals(m)
+
+
+def source_to_out(source, tokenizer, weights, d_model, n_heads, head):
+    """ OV circuit for a single head. """
     d_head = d_model // n_heads
 
     tok = tokenizer.encode(source)
     if len(tok) > 1:
         raise ValueError("source must be a single token")
     
-    x = weights['embedding.weight'][tok]
+    h_w = get_weights_for_head(weights, head, n_heads, d_model)
 
-    w_v = weights['attn.attn.in_proj_weight'][2*d_model:] 
-    w_o = weights['attn.attn.out_proj.weight'] # shape (n_heads*d_model/n_heads, d_model)
+    x = h_w['w_e'][:, tok]
 
-    v = torch.matmul(x, w_v)
+    # x = (x - np.average(x)) / np.std(x)
+    # x = x*h_w['ln1w'] + h_w['ln1b']
+    v = h_w['w_v'] @ x
+    o = h_w['w_o'] @ v
+    o = (o - np.average(o)) / np.std(o)
+    o = o*h_w['lnfw'] + h_w['lnfb']  # ???
+    y = h_w['w_u'] @ o
 
-    outs = []
-    for h in range(n_heads):
-        v_h = v[:, h*d_head: (h+1)*d_head]
-        w_o_h = w_o[h*d_head: (h+1)*d_head, :]
-        o = torch.matmul(v_h, w_o_h)
-        outs.append(o.squeeze(0))
-    
-    out = torch.stack(outs, dim=0)
-
-    # unembed!
-    w_u = weights['unembedding.weight']
-    out_tokens = torch.matmul(out, w_u.T)
-
-    top = torch.topk(out_tokens, 5, dim=1)
-    top_tokens = top.indices
-
-    # decoder needs list of int
-    tokens = [h.tolist() for h in top_tokens]
-
-    for i, h in enumerate(tokens):
-        print(tokenizer.decode_tokens_bytes(h))
-        print(top.values[i].tolist())
-        print()
+    torch_y = torch.from_numpy(y).squeeze(1)
+    top = torch.topk(torch_y, 5)
+    print(tokenizer.decode_tokens_bytes(top.indices.tolist()))
+    print(top.values.tolist())
 
 
 if __name__=="__main__":
     # train()
 
     enc = tiktoken.get_encoding("gpt2")
-    weights = torch.load("out/from_odin/small_8000.pt", map_location='cpu')
+    weights = torch.load("out/from_odin/big_22000.pt", map_location='cpu')
+    # weights = torch.load("out/from_odin/small_8000.pt", map_location='cpu')
 
     for weight in weights:
         print(weight, weights[weight].shape)
@@ -150,18 +177,40 @@ if __name__=="__main__":
     # model = OneLayerAttnTransformer(config.model)
     # model.load_state_dict(weights)
 
-    # idxs = enc.encode("Hello, my name is")
+    # idxs = enc.encode(" hello")
     # in_batch = torch.tensor(idxs).unsqueeze(0)
     # generated = model.generate(in_batch, max_new_tokens=10)
     # print(enc.decode_tokens_bytes(generated[0].tolist()))
 
 
+    graphs = []
+    for head in range(n_heads):
+        eigen = get_eigenvalues(weights=weights, head=head, n_heads=n_heads, d_model=d_model)
+        xs = eigen.real
+        ys = eigen.imag
+        graphs.append((xs, ys))
 
-    with torch.no_grad():
-        source_to_out_token(
-            " github",
-            tokenizer=enc,
-            weights=weights,
-            d_model=d_model,
-            n_heads=n_heads,
-        )
+    n_rows = n_heads // 2
+    fig, ax = plt.subplots(2, n_rows, subplot_kw={'projection': 'polar'})
+    for i, (xs, ys) in enumerate(graphs):
+        axis = ax[i//n_rows, i%n_rows]
+        axis.scatter(np.angle(xs + 1j*ys), np.log(np.abs(xs + 1j*ys)))
+        axis.set_xticks([])
+        # axis.set_yticks([])
+        axis.set_xlabel('')
+        axis.set_ylabel('')
+        axis.set_title('')
+    fig.tight_layout()
+    plt.show()
+
+
+    # for h in range(n_heads):
+    #     print("head", h)
+    #     source_to_out(
+    #         " perfect",
+    #         tokenizer=enc,
+    #         weights=weights,
+    #         d_model=d_model,
+    #         n_heads=n_heads,
+    #         head=h,
+    #     )

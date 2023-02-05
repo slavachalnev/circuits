e2c7f56346fcd4d5f919399cef82cea22be98347
Slava Chalnev <slava.chalnev@gmail.com>
2023-02-05 16:22:30 +1100

    Get QK circuit

--- experiments/one_layer_attn.py
+++ experiments/one_layer_attn.py
@@ -10,6 +10,8 @@ import tiktoken
 
 from matplotlib import pyplot as plt
 
+from tqdm import tqdm
+
 from torch.utils.tensorboard import SummaryWriter
 
 from circuits.models.one_attn_layer import OneLayerAttnTransformer
@@ -25,7 +27,7 @@ def get_config():
     # system
     C.system = CN()
     C.system.seed = 3407
-    C.system.work_dir = './out/one_layer_openwebtext_big'
+    C.system.work_dir = './out/small_nolnf_nobias'
 
     # model
     C.model = OneLayerAttnTransformer.get_default_config()
@@ -37,7 +39,7 @@ def get_config():
     C.trainer = Trainer.get_default_config()
     C.trainer.block_size = 2048
     C.trainer.batch_size = 32
-    C.trainer.micro_batch_size = 4
+    # C.trainer.micro_batch_size = 4
 
     C.trainer.learning_rate = 2e-4
     C.trainer.decay_lr = True
@@ -104,6 +106,11 @@ def get_weights_for_head(weights, head, n_heads, d_model, norm_emb=True):
     w_v_h = w_v[head*d_head: (head+1)*d_head, :]
     w_o_h = w_o[:, head*d_head: (head+1)*d_head]
 
+    w_q = weights['attn.attn.in_proj_weight'][:d_model]
+    w_k = weights['attn.attn.in_proj_weight'][d_model:2*d_model]
+
+    w_q_h = w_q[head*d_head: (head+1)*d_head, :]
+    w_k_h = w_k[head*d_head: (head+1)*d_head, :]
 
     ln1w = weights['attn.ln.weight'].unsqueeze(1).numpy()
     ln1b = weights['attn.ln.bias'].unsqueeze(1).numpy()
@@ -120,8 +127,8 @@ def get_weights_for_head(weights, head, n_heads, d_model, norm_emb=True):
         'w_u': weights['unembedding.weight'].numpy(),
         'ln1w': ln1w,
         'ln1b': ln1b,
-        'lnfw': weights['ln_f.weight'].unsqueeze(1).numpy(),
-        'lnfb': weights['ln_f.bias'].unsqueeze(1).numpy(),
+        'w_q': w_q_h.numpy(),
+        'w_k': w_k_h.numpy(),
     }
 
 
@@ -132,25 +139,17 @@ def get_eigenvalues(weights, head, n_heads, d_model):
     return np.linalg.eigvals(m)
 
 
-def source_to_out(source, tokenizer, weights, d_model, n_heads, head):
+def source_to_out(source, tokenizer, head_weights):
     """ OV circuit for a single head. """
-    d_head = d_model // n_heads
-
     tok = tokenizer.encode(source)
     if len(tok) > 1:
         raise ValueError("source must be a single token")
     
-    h_w = get_weights_for_head(weights, head, n_heads, d_model)
+    x = head_weights['w_e'][:, tok]
 
-    x = h_w['w_e'][:, tok]
-
-    # x = (x - np.average(x)) / np.std(x)
-    # x = x*h_w['ln1w'] + h_w['ln1b']
-    v = h_w['w_v'] @ x
-    o = h_w['w_o'] @ v
-    o = (o - np.average(o)) / np.std(o)
-    o = o*h_w['lnfw'] + h_w['lnfb']  # ???
-    y = h_w['w_u'] @ o
+    v = head_weights['w_v'] @ x
+    o = head_weights['w_o'] @ v
+    y = head_weights['w_u'] @ o
 
     torch_y = torch.from_numpy(y).squeeze(1)
     top = torch.topk(torch_y, 5)
@@ -158,12 +157,47 @@ def source_to_out(source, tokenizer, weights, d_model, n_heads, head):
     print(top.values.tolist())
 
 
+def source_to_dest(source, tokenizer, head_weights, head):
+    """ QK circuit for a single head. """
+    tok = tokenizer.encode(source)
+    if len(tok) > 1:
+        raise ValueError("source must be a single token")
+
+    x = head_weights['w_e'][:, tok]
+    k = head_weights['w_k'] @ x
+    kq = head_weights['w_q'].T @ k
+    dst = head_weights['w_e'].T @ kq
+
+    qk_averages = np.load(f'qk_big_nolnf_nobias/qk_averages_{head}.npy')
+    # subtract the average qk value for each query
+    dst = dst.squeeze(1) - np.array(qk_averages)
+
+    # reweight by token frequency
+    freq = np.load('openwebtext_gpt2_averages.npy')
+    dst = dst * (freq**0.1)
+
+    tdst = torch.from_numpy(dst)
+    top = torch.topk(tdst, 5)
+    print(tokenizer.decode_tokens_bytes(top.indices.tolist()))
+    print(top.values.tolist())
+
+def save_qk_averages_for_head(head_weights, head):
+    """ Compute and save the average qk value for each query. """
+    qk_averages = []
+    for i in tqdm(range(head_weights['w_e'].shape[1])):
+        x = head_weights['w_e'][:, i]
+        q = head_weights['w_q'] @ x
+        qk = head_weights['w_k'].T @ q
+        src = head_weights['w_e'].T @ qk
+        qk_averages.append(src.mean())
+    np.save(f"qk_averages_{head}", np.array(qk_averages))
+
 if __name__=="__main__":
     # train()
 
     enc = tiktoken.get_encoding("gpt2")
-    weights = torch.load("out/from_odin/big_22000.pt", map_location='cpu')
-    # weights = torch.load("out/from_odin/small_8000.pt", map_location='cpu')
+    # weights = torch.load("out/from_odin/big_22000.pt", map_location='cpu')
+    weights = torch.load("out/from_odin/big_nolnf_nobias_20000.pt", map_location='cpu')
 
     for weight in weights:
         print(weight, weights[weight].shape)
@@ -172,11 +206,15 @@ if __name__=="__main__":
     n_heads = config.model.n_head
     d_model = config.model.n_embd
 
-    # # construct the model
+    # # compute average qk values for each head.
+    # for h in range(n_heads):
+    #     h_w = get_weights_for_head(weights, h, n_heads, d_model)
+    #     save_qk_averages_for_head(h_w, h)
+
+    # # construct a model and generate some text
     # config.model.block_size = config.trainer.block_size
     # model = OneLayerAttnTransformer(config.model)
     # model.load_state_dict(weights)
-
     # idxs = enc.encode(" hello")
     # in_batch = torch.tensor(idxs).unsqueeze(0)
     # generated = model.generate(in_batch, max_new_tokens=10)
@@ -196,21 +234,26 @@ if __name__=="__main__":
         axis = ax[i//n_rows, i%n_rows]
         axis.scatter(np.angle(xs + 1j*ys), np.log(np.abs(xs + 1j*ys)))
         axis.set_xticks([])
-        # axis.set_yticks([])
         axis.set_xlabel('')
         axis.set_ylabel('')
         axis.set_title('')
     fig.tight_layout()
     plt.show()
 
+    print()
+    word = "www"
+    print('word:', word)
 
-    # for h in range(n_heads):
-    #     print("head", h)
-    #     source_to_out(
-    #         " perfect",
-    #         tokenizer=enc,
-    #         weights=weights,
-    #         d_model=d_model,
-    #         n_heads=n_heads,
-    #         head=h,
-    #     )
+    for h in range(n_heads):
+        h_w = get_weights_for_head(weights, h, n_heads, d_model)
+        print()
+        print("head", h)
+        print("source to out")
+        source_to_out(
+            word,
+            tokenizer=enc,
+            head_weights=h_w,
+        )
+        print("source to dest")
+        source_to_dest(source=word, tokenizer=enc, head_weights=h_w, head=h)
+
